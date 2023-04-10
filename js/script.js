4a4281a911aa8fb4e92eb300172ddfd20cbab820
ThiagoP15 <thiporto84@gmail.com>
2023-04-10 13:17:38 -0300

    1.0

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const formNome = document.querySelector('nome')
+const formIdade = document.querySelector('idade')
+const formBiografia = document.querySelector('biografia')
+const formLocalizacao = document.querySelector('localizacao')
+
