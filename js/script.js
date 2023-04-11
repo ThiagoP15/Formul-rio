d8984bb2b14b0b480b325523ae918e45e5f1b0d3
ThiagoP15 <thiporto84@gmail.com>
2023-04-11 03:13:34 -0300

    adicionando css

--- js/script.js
+++ js/script.js
@@ -1,46 +1,18 @@
+const perfil = document.querySelector("perfil")
 const form = document.getElementById('update-form');
 const updateBtn = document.getElementById('update-btn');
 
-form.addEventListener("submit", (campo) => {
-    campo.preventDefault()
-    const atualiza ={
-     "nome": campo.target.elements['nome'].value,
-     "idade": campo.target.elements['idade'].value,
-     "biografia": campo.target.elements['biografia'].value,
-     "localizacao": campo.target.elements['localiza'].value,
-    }
-    localStorage.setItem("atualizar", JSON.stringify(atualiza))
-})
-function atualizaElemento() {
-    console.log(nome)
-    console.log(idade)
-    console.log(biografia)
-    console.log(localiza)
-
-    
+function alteraDados () {
+    var nome = document.getElementById('nome').value
+    var idade = document.getElementById('idade').value
+    var bio = document.getElementById('biografia').value
+    var local = document.getElementById('localiza').value
 }
-    perfil.innerHtml = `
-    <main class="perfil" id="perfil" data-perfil>
-    <div id="imagem_de_perfil">
-     <h1>Imagem de Perfil</h1>
-     <p id="imagemP">
-         <img src="./imagens/Profile-Avatar-PNG-Picture.png">
-     </p>
-    </div>
-    <div id="nomeP">
-     <h2>${}</h2>
-    </div>
-    <div id="idadeP">
-     <h3>${}</h3>
-    </div>
-    <div id="localizacaoP">
-     <h4>${}</h4>
-    </div>
-    <div id="biografiaP">
-     <h5>Biografia</h5>
-     <p id="bioP">
-        ${}
-     </p>
-    </div>
- </main>
-    `
+function atualiza(perfil) {
+    perfil.innerHTML = `<div id="nomeP">${nome}</div>`
+    perfil.innerHTML = `<div id="idadeP">${idade}</div>`
+    perfil.innerHTML = `<div id="biografiaP">${bio}</div>`
+    perfil.innerHTML = `<div id="localizacaoP">${local}</div>`
+
+    return atualiza;
+}
