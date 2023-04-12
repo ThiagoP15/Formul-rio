a327c0571df9804875e26afda9b1d70e5272bd2e
ThiagoP15 <thiporto84@gmail.com>
2023-04-12 14:07:52 -0300

    terminando o formulário

--- js/script.js
+++ js/script.js
@@ -2,29 +2,47 @@
 const perfil = document.getElementById("perfil");
 const form = document.getElementById("update-form");
 const btn = document.getElementById("update-btn");
-    form.addEventListener("submit", (evento) => {
-        evento.preventDefault()
-        const formulario = {
-         "nome": evento.target.elements['nome'].value,
-         "idade": evento.target.elements['idade'].value,
-         "local": evento.target.elements['localiza'].value,
-         "bio": evento.target.elements['bio'].value,
-         "email": evento.target.elements['email'].value
-        }
-                
-        localStorage.setItem("dados", JSON.stringify(formulario))
 
-        const dados = JSON.parse(localStorage.getItem("dados"));
+const nomeElemento = document.getElementById("nomeP");
+const idadeElemento = document.getElementById("idadeP");
+const localElemento = document.getElementById("localizacaoP");
+const bioElemento = document.getElementById("biografiaP");
+const emailElemento = document.getElementById("emailP");
 
-        const nomeElemento = document.getElementById("nomeP");
-        const idadeElemento = document.getElementById("idadeP");
-        const localElemento = document.getElementById("localizacaoP");
-        const bioElemento = document.getElementById("biografiaP");
-        const emailElemento = document.getElementById("emailP");
-        nomeElemento.textContent = " " + dados.nome;
-        idadeElemento.textContent = " " + dados.idade;
-        localElemento.textContent = " " + dados.local;
-        bioElemento.textContent = dados.bio;
-        emailElemento.textContent = " " + dados.email;
+form.addEventListener("submit", (evento) => {
+  evento.preventDefault();
 
-            })
+  const formulario = {
+    "nome": evento.target.elements['nome'].value,
+    "idade": evento.target.elements['idade'].value,
+    "local": evento.target.elements['localiza'].value,
+    "bio": evento.target.elements['bio'].value,
+    "email": evento.target.elements['email'].value
+  };
+  
+  localStorage.setItem("dados", JSON.stringify(formulario));
+  
+  const dados = JSON.parse(localStorage.getItem("dados"));
+
+  // atualiza os elementos HTML com os valores armazenados
+  nomeElemento.textContent = "Nome: " + dados.nome;
+  idadeElemento.textContent = "Idade: " + dados.idade;
+  localElemento.textContent = "Localização: " + dados.local;
+  bioElemento.textContent = dados.bio;
+  emailElemento.textContent = "Email: " + dados.email;
+
+  // limpa os campos do formulário
+  evento.target.reset();
+});
+
+// verifica se há dados armazenados e preenche os elementos HTML com os valores
+const dados = JSON.parse(localStorage.getItem("dados"));
+
+if (dados) {
+  nomeElemento.textContent = "Nome: " + dados.nome;
+  idadeElemento.textContent = "Idade: " + dados.idade;
+  localElemento.textContent = "Localização: " + dados.local;
+  bioElemento.textContent = dados.bio;
+  emailElemento.textContent = "Email: " + dados.email;
+}
+
