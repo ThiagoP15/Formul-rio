1dbd0fb5eb5e76da9869d275ab8bddeb6d2dcf9f
ThiagoP15 <thiporto84@gmail.com>
2023-04-13 14:14:37 -0300

    terminando o projeto

--- js/script.js
+++ js/script.js
@@ -43,6 +43,24 @@ form.addEventListener("submit", (evento) => {
   // limpa os campos do formulário
   evento.target.reset();
 });
+  // formata o número de telefone
+const input = document.getElementById("tel");
+
+input.addEventListener("keyup", formatarTelefone);
+
+function formatarTelefone(e){
+
+var v=e.target.value.replace(/\D/g,"");
+
+v=v.replace(/^(\d\d)(\d)/g,"($1)$2"); 
+
+v=v.replace(/(\d{5})(\d)/,"$1-$2");    
+
+e.target.value = v;
+
+ 
+
+}
 
 
 
